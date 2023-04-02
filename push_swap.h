db4f64bfcc0f5fc53dbc0165dd22e8c912edb707
Reira Takashima <rtakashi@c1r21s5.42tokyo.jp>
2023-04-02 22:27:51 +0900

    add from clusters

---
+++ push_swap.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rtakashi <rtakashi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/02 21:36:12 by rtakashi          #+#    #+#             */
+/*   Updated: 2023/04/02 21:43:35 by rtakashi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <fcntl.h>
+# include <limits.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int	ft_atoi_intver(const char *str);
+
+#endif
